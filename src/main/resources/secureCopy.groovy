25bc8aa9bccf93996149b4c1cb69c3f3eb5b4ce6
abipster <abipster.6@gmail.com>
2017-03-02 01:50:57 +0000

    added YumRemoteServer class

--- src/main/resources/secureCopy.groovy
+++ src/main/resources/secureCopy.groovy
@@ -2,25 +2,47 @@ stage('nightly'){
 
     def yumUser = "root"
     def yumHost = "172.28.23.224"
-    
-    def remoteExecutor = new RemoteExecutor(yumUser, yumHost)  
-    remoteExecutor.runRemoteCommand("mkdir /datacentre/0.1.0.1234/ingg")
+    def svnRevision = "0.1.0.1234"
 
+    def remoteExecutor = new RemoteYumServer(yumUser, yumHost)  
+    echo remoteExecutor.getRunnableCommand("mkdir -p /var/www/CoreHub/nightly/${svnRevision}/datacentre/ingg")
+
+    setupYumDirs(remoteExecutor, svnRevision)
 }
 
+def setupYumDirs(remoteYumServer, svnRevision){
 
+    for(String module in remoteYumServer.dirStructure){
+        echo "module.key = ${module.key}, module.value = ${module.value}"
 
-class RemoteExecutor{
+        for(String dir in remoteYumServer.dirStructure[module.key]){
+            echo remoteYumServer.getRunnableCommand("mkdir -p /var/www/CoreHub/nightly/${svnRevision}/${module.key}/${dir}")
+        }
+    }
+}
+
+enum YumModule {datacentre, site, stub}
+
+class RemoteYumServer{
 
     String user
     String host
+    Map<String, List> dirStructure = [:]
 
-    RemoteExecutor(user, host){
+    RemoteYumServer(user, host){
         this.user = user
         this.host = host
+
+        def datacentreDirs = ["ingg", "thirdparty", "database"]
+        def siteDirs = ["ingg", "thirdparty"]
+        def stubDirs = ["ingg", "thirdparty", "stub"]
+        dirStructure.'datacentre'= datacentreDirs
+        dirStructure.'site'= siteDirs
+        dirStructure.'stub'= stubDirs
+        
     }
 
-    def runRemoteCommand(command){
-        echo "ssh -o StrictHostKeyChecking=no ${user}@${host} ${command}"
+    def getRunnableCommand(command){
+        return "ssh -o StrictHostKeyChecking=no ${user}@${host} ${command}"
     }
 }
